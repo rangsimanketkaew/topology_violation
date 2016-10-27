f211cf51925b9dcaefec87dd1eadd7f848d6890f
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2016-10-27 16:46:23 +0700

    Updated
    debugging

--- tv_count.py
+++ tv_count.py
@@ -1,18 +1,24 @@
-## Polymer structure analysis tool
-## --> Topology Violation counting, version.1 (debugging)
-## The calculation of the topology violation of the polymer system
-## The code is writen by Rangsiman Ketkaew, Computational Chemistry Research Unit, Thammasat U., Thailand (Oct 2016)
-## step*.txt is the file that include the coordinate data of fictitious beads during simulation 
+## Polymer Structure Analysis Tool
+## --> Coding Script for Counting The Number of Topology Violation, version 1.1 (Oct 2016, debugging)
+## --> This script is improved interface for general user.
+## --> The code is writen by Rangsiman Ketkaew, Computational Chemistry Research Unit, Thammasat U., Thailand
+## --> Contact me for what you want to see in next project, rangsiman1993(at)gmail(dot)com
 
-#import scientific things from python
 import numpy as np
 import math
 
-#You can (must) change N and B as integer number
-N = 5 # Number of files ## <--- Double check before submit # The file has to be in the same place with the input file
-B = 24 # Number of Bead/Particle ## <--- Double check before submit
+## You can (must) change N and B to satify your computation (integer number)
+## The name of coor file need to be in format of " step*.txt ", where * is start from 0, 1, 2, and so on.
+        # The general format of coor file is  contains 5 column # You can see the example coor file in folder /example/
+        # 0th column is number of bead/molecule
+        # 1st column is the number of polymer chain
+        # 2nd column is the coor of particle on X axis
+        # 3rd column is the coor of particle on Y axis
+        # 4th column is the coor of particle on Z axis
+        # 5th column is empty
 
-rB = B - 1
+N = 10 # Number of coor file ## <--- Need to check before submit
+B = 2145 # Number of Bead/Molecule ## <--- Need to check before submit
 
 def findDist(x,y):
     delta = np.subtract(x,y)
@@ -23,7 +29,7 @@ def findDist(x,y):
 
 numFile = 0
 
-while numFile < N :  # Change size of data files that u need to read.
+while numFile < N :
 
     fileName = "step"+str(numFile)+".txt"  # string of filename
     f = open(fileName,'r')
@@ -32,7 +38,7 @@ while numFile < N :  # Change size of data files that u need to read.
 
     for line in f :
         v = line.split()
-        values = map(float,v[2:5]) # the position of X,Y,X coordinate
+        values = map(float,v[2:5])  # the position of X,Y,X coordinate # extract only the coor (xyz) in column 2nd-5th
         vector_list.append(values)
 
     # This line finished to read and store data to list.
@@ -42,8 +48,8 @@ while numFile < N :  # Change size of data files that u need to read.
     g = open(fileName,'w')
     #g2 = open("output2.txt",'w') # no save
 
-    vector_raw = vector_list[0:rB] ## vector_list[x:y] is bead no. x --> no. y
-    # if u need to use all of data in a file, u can use vector_list to run this code.
+    vector_raw = vector_list[0:B] ## vector_list[x:y] is bead no. x --> no. y
+    # if you need to use all of data in a file, you can use vector_list to run this code.
 
     i = 0
 
@@ -52,13 +58,12 @@ while numFile < N :  # Change size of data files that u need to read.
         while j < len(vector_raw) :
             dist = findDist(vector_raw[i], vector_raw[j])
 
-            if dist <= 0.8 : ## 1.0 = global cut-off
+            if dist <= 0.8 : ## 1.0 = global cut-off <-- You can flexibly change the distance of cutoff.
                 ## case one distance <= 1.0
                 a = np.subtract(vector_raw[j],vector_raw[i])
                 result = "%d -> %d , %f %f %f \n" % (i, j, a[0], a[1], a[2])
 
                 g.write(result)
-
 
             #elif dist > 0.8 :
                 ## case two distance > 1.0
@@ -75,11 +80,9 @@ while numFile < N :  # Change size of data files that u need to read.
     # update numFile (number of file data)
     numFile += 1
 
-#print 'Done'
+print " ---- Wow, wait a minute ! ---- "
 
-
-## Second job
-## Dot product of two vectors and finding alpha (in degree unit)
+## Second job, dot product of two vectors and finding alpha (in degree unit)
 
 import numpy as np
 import math
@@ -162,6 +165,5 @@ print " Total of Event = " "%s" % total
 print " ------ Normal termination ------ "
 
 print " -------- See You Again --------- "
-
 
 ## The computation has done ##
