ae9c830b18c9927b6f5279d8f98610a47793458d
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2016-10-27 16:44:56 +0700

    Update tv_script.sh

--- tv_script.sh
+++ tv_script.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-#-----------------------------------------#
 cd .
+
 # nohup mpirun -np N python tv_count.py &
 # where N is the number of the processor for MPI (N = positive integer, says, N=4)
+
 nohup mpirun -np 1 python tv_count.py &
 wait
-mv -f ./*output.txt ./output # Pleae create output folder beforeward
+mv -f *output.txt ./output # Pleae create /output/ folder beforeward
 wait
-mv -f ./count.txt ./count # Please create count folder beforeward
+mv -f count.txt ./count # Please create /count/ folder beforeward
 
-#-----------------------------------------#
-
-echo "Job done"
+echo "----Job done----"
